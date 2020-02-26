a839ad614be88dfe06acf681a2fcd36af17cd424
Aleksander Jaworski <aleck156@gmail.com>
2020-02-26 10:04:25 +0100

    registered PostUpdateView in urls.py

--- private_site/blog/urls.py
+++ private_site/blog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name = 'post_list'),
     path('about/', views.AboutView.as_view(), name='about'),
     path('post/<int:pk>', views.PostDetailView.as_view(), name = 'post_detail'),
-    path('post/new/', views.CreatePostView.as_view(), name = 'post_new')
+    path('post/new/', views.CreatePostView.as_view(), name = 'post_new'),
+    path('post/update/<int:pk>/edit', views.PostUpdateView.as_view(), name = 'post_edit' ),
 ]
