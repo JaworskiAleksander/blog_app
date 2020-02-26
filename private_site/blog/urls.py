964b2f2465275f32f8ce6978fc648dd1f3b91f8d
Aleksander Jaworski <aleck156@gmail.com>
2020-02-26 09:49:45 +0100

    registered PostDetailView under post/<int:pk> alias urlpattern app

--- private_site/blog/urls.py
+++ private_site/blog/urls.py
@@ -7,5 +7,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.PostListView.as_view(), name = 'post_list'),
     path('about/', views.AboutView.as_view(), name='about'),
-    
+    path('post/<int:pk>', views.PostDetailView.as_view(), name = 'post_detail')    
 ]
