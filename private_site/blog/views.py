ed22ab513bdfedaf8f7d660536b51501c2dcfaa5
Aleksander Jaworski <aleck156@gmail.com>
2020-02-26 10:16:45 +0100

    created DraftListView, fixed parenthesis typo in Post.objects.filter in PostListView

--- private_site/blog/views.py
+++ private_site/blog/views.py
@@ -18,7 +18,7 @@ class PostListView(ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte = timezone.now().order_by('-published_date'))
+        return Post.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')
 
 class PostDetailView(DetailView):
     model = Post
@@ -39,3 +39,12 @@ class PostUpdateView(UpdateView, LoginRequiredMixin):
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(publish_date__isnull=True).order_by('create_date')
+    
