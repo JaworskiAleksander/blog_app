c304307cb16043b7b584b27c605ff20d3d05dd8f
Aleksander Jaworski <aleck156@gmail.com>
2020-02-26 10:11:38 +0100

    registered PostDeleteView in urls.py

--- private_site/blog/urls.py
+++ private_site/blog/urls.py
@@ -6,8 +6,9 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name = 'post_list'),
-    path('about/', views.AboutView.as_view(), name='about'),
+    path('about/', views.AboutView.as_view(), name = 'about'),
     path('post/<int:pk>', views.PostDetailView.as_view(), name = 'post_detail'),
     path('post/new/', views.CreatePostView.as_view(), name = 'post_new'),
-    path('post/update/<int:pk>/edit', views.PostUpdateView.as_view(), name = 'post_edit'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name = 'post_edit'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name = 'post_delete'),
 ]
